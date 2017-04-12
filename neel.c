a8b6a949b6040a29c2c546141c9e13b08a25f455
neel <neilmenpara1998@gmail.com>
2017-04-13 01:05:11 +0530

    final commit

--- neel.c
+++ neel.c
@@ -1,2 +1,11 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
 
+int main(int n, char *chr[])
+{
 
+printf(1,"%s %s \n",chr[0],chr[1]);
+exit();
+
+}
---
+++ sleep.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+
+int main(int n, char *chr[])
+{
+int p,m;
+p=atoi(chr[1]);
+m=p*1000/14;
+printf(1,"wait for %d seconds\n",p);
+sleep(m);
+printf(1," so wait over");
+exit();
+}
