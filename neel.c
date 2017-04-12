d5099577f3c34f9290d1c49f258dffe3afbbfb6d
neel <neilmenpara1998@gmail.com>
2017-04-13 00:27:52 +0530

    my first backup

---
+++ neel.c
@@ -0,0 +1,21 @@
+// A simple program which just prints something on screen
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+ main(int n , char *chr[])
+{
+
+
+if(n<=1)
+{
+exit();
+}
+
+printf(1,"%s %s\n",chr[0],chr[1]);
+
+exit();
+
+}
